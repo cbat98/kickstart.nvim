722fbccd94bbf816d070d6ea95892f4e7f706c81
Charlie Batten <charlie.batten@microlise.com>
2024-03-08 16:36:09 +0000

    Add lazygit plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -41,4 +41,14 @@ return {
       vim.keymap.set('n', 'S', '<Plug>(leap-backward)')
     end,
   },
+
+  {
+    'kdheepak/lazygit.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      vim.keymap.set('n', '<leader>lg', vim.cmd.LazyGit, { desc = 'Open [L]azy[G]it' })
+    end,
+  },
 }
