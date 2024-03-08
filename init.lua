a1477c892d5074142094c6aa48699729025470c6
Charlie Batten <charlie.batten@microlise.com>
2024-03-08 13:33:51 +0000

    Added markdownlint-cli2

--- init.lua
+++ init.lua
@@ -596,6 +596,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format lua code
+        'markdownlint-cli2', -- Used to format markdown documents
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
