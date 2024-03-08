a21e014a83b0c6037b747312f350200ab9a41c4e
Charlie Batten <charlie.batten@microlise.com>
2024-03-08 13:56:04 +0000

    Added null_ls

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,17 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- none-ls (community fork of null-ls) for linting
+  'nvimtools/none-ls.nvim',
+  event = 'VimEnter',
+  config = function()
+    local null_ls = require 'null-ls'
+    null_ls.setup {
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.diagnostics.markdownlint_cli2.with { args = { '$FILENAME' } },
+      },
+    }
+  end,
+}
