bc0d89cab64e50abe3af1d10f1d8942101a70e6d
Charlie Batten <charlie.batten@microlise.com>
2024-03-08 15:12:15 +0000

    Added leap

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -31,4 +31,12 @@ return {
       vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
     end,
   },
+  {
+    'ggandor/leap.nvim',
+    event = 'VeryLazy',
+    config = function()
+      vim.keymap.set('n', 's', '<Plug>(leap-forward)')
+      vim.keymap.set('n', 'S', '<Plug>(leap-backward)')
+    end,
+  },
 }
