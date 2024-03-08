98f991fbf89d1e5a7befe34cf015a81c53b63f81
Charlie Batten <charlie.batten@microlise.com>
2024-03-08 14:25:07 +0000

    Add peek nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,16 +3,32 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  -- none-ls (community fork of null-ls) for linting
-  'nvimtools/none-ls.nvim',
-  event = 'VimEnter',
-  config = function()
-    local null_ls = require 'null-ls'
-    null_ls.setup {
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.markdownlint_cli2.with { args = { '$FILENAME' } },
-      },
-    }
-  end,
+  {
+    -- none-ls (community fork of null-ls) for linting
+    'nvimtools/none-ls.nvim',
+    event = 'VimEnter',
+    config = function()
+      local null_ls = require 'null-ls'
+      null_ls.setup {
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.diagnostics.markdownlint_cli2.with { args = { '$FILENAME' } },
+        },
+      }
+    end,
+  },
+  {
+    'toppair/peek.nvim',
+    event = 'VeryLazy',
+    build = 'deno task --quiet build:fast',
+    config = function()
+      require('peek').setup {
+        auto_load = false,
+        theme = 'light',
+        app = 'browser',
+      }
+      vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+      vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+    end,
+  },
 }
