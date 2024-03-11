1c4f5b74a2afe61280182ec99d032f7569d0e828
Charlie Batten <charlie.batten@microlise.com>
2024-03-11 11:45:50 +0000

    Comment out tokyonight theme

--- init.lua
+++ init.lua
@@ -85,7 +85,7 @@ P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
 -- Set variable for AppData\Local
-local localAppData = os.getenv('localappdata') or ""
+local localAppData = os.getenv 'localappdata' or ''
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
@@ -740,23 +740,25 @@ require('lazy').setup({
     end,
   },
 
+  -- NOTE: The color scheme has been disabled here to allow a cusom one to take priority
+
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     'folke/tokyonight.nvim',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
+    -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    -- priority = 1000, -- make sure to load this before all the other start plugins
+    -- config = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like
-      vim.cmd.hi 'Comment gui=none'
-    end,
+      -- vim.cmd.hi 'Comment gui=none'
+    -- end,
   },
 
   -- Highlight todo, notes, etc in comments
