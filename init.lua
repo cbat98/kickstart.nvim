112d4c4187c354b454b06ec1bf79a79c88186747
Charlie Batten <charlie.batten@microlise.com>
2024-04-11 09:12:23 +0100

    OTBS for powershell

--- init.lua
+++ init.lua
@@ -586,6 +586,7 @@ require('lazy').setup({
         omnisharp = {},
         powershell_es = {
           bundle_path = localAppData .. '/nvim-data/mason/packages/powershell-editor-services',
+          settings = { powershell = { codeFormatting = { Preset = 'OTBS' } } },
         },
       }
 
@@ -773,13 +774,13 @@ require('lazy').setup({
     -- 'folke/tokyonight.nvim',
     -- priority = 1000, -- Make sure to load this before all the other start plugins.
     -- init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'tokyonight-night'
+    -- Load the colorscheme here.
+    -- Like many other themes, this one has different styles, and you could load
+    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    -- vim.cmd.colorscheme 'tokyonight-night'
 
-      -- You can configure highlights by doing something like:
-      -- vim.cmd.hi 'Comment gui=none'
+    -- You can configure highlights by doing something like:
+    -- vim.cmd.hi 'Comment gui=none'
     -- end,
   },
 
