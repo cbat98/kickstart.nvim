3a5474b67578e229d73b16e995fe74c140e2fa38
Charlie Batten <charlie@charliebatten.co.uk>
2024-05-15 20:53:23 +0100

    Add oil nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -66,4 +66,14 @@ return {
       vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
     end,
   },
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    init = function()
+      require('oil').setup {
+        columns = { 'icon' },
+      }
+      vim.keymap.set('n', '-', vim.cmd.Oil, { desc = 'Oil' })
+    end,
+  },
 }
