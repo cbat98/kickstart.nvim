2528ac986eb061ad03e19c4da9ac43b8d5570463
Charlie Batten <charlie.batten@microlise.com>
2024-05-16 14:44:10 +0100

    Removed leader pv

--- init.lua
+++ init.lua
@@ -205,9 +205,6 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- Keybind to show Explorer
-vim.keymap.set('n', '<leader>pv', vim.cmd.Explore, { desc = 'Show Explorer' })
-
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
