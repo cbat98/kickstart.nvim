2e38fb2611d2bda43b843274352ad376e890935d
Charlie Batten <charlie.batten@microlise.com>
2024-05-31 09:04:24 +0100

    Removed ps1 from format-on-save

--- init.lua
+++ init.lua
@@ -667,7 +667,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true, cs = true }
+        local disable_filetypes = { c = true, cpp = true, cs = true, ps1 = true }
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
