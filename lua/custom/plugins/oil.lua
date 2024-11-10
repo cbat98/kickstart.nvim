19bf0a192ff5c6b177aa93b64da72794589b5889
Charlie Batten <charlie.batten@microlise.com>
2024-11-10 10:42:21 +0000

    Show hidden items in oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -4,6 +4,9 @@ return {
   init = function()
     require('oil').setup {
       columns = { 'icon' },
+      view_options = {
+        show_hidden = true,
+      },
     }
     vim.keymap.set('n', '-', vim.cmd.Oil, { desc = 'Oil' })
   end,
