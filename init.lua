fed0947bd13f79f2e284ccea6c3ab92294934955
Charlie Batten <charlie.batten@microlise.com>
2025-06-11 16:06:27 +0100

    Set contrast to soft

--- init.lua
+++ init.lua
@@ -906,7 +906,9 @@ require('lazy').setup({
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = true,
+    opts = {
+      contrast = 'soft',
+    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
